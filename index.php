0d35e06b8e4b35b42832562131d049a4386ef309
gaykarkiransantosh <kiran.gaykar@somaiya.edu>
2021-01-30 18:22:19 +0530

    sccess

---
+++ index.php
@@ -0,0 +1,204 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>UI DESIGN</title>
+  </head>
+  <body>
+
+      <div class="container first" >
+<div class="header">
+    <nav class="navbar  justify-content-between">
+        <a class="navbar-brand col-1 font-weight-bold">elearning</a>
+        <a class="navbar-brand col-1">For Students</a>
+        <a class="navbar-brand col-1">For Bussines</a>
+        <form class="form-inline ">
+            <a class="navbar-brand col-3">Log in</a>
+          <button type="button" class="btn btn-dark col-5">Get Started</button>
+          <span ><i class="fa fa-search col-2"></i></span>
+        </form>
+      </nav>
+</div>
+<div class="container">
+<div class="container">
+    <div class="row">
+        <div class="col-7" id="first-container-half">
+            <div class="container" >
+<div class="row rc">
+    <span class="font-weight-bold " id="font-first">World-class <br>learning for <br> anyone,anywhere</span>
+
+</div>
+<div class="row rc">
+    <span>Build Skills with courses,certificates,and degrees online from world-classs universities and companies</span>
+   
+</div>
+<div class="row rc">
+  
+            <button type="button " class="btn btn-dark col-5 rc1">Search anything            <span   ><i class="fa fa-search col-3"></i></span></button>
+        
+ 
+    
+</div>
+            </div>
+        </div>
+        <div class="col-5" id="second-container-half">
+            <img src="img/stu1.png" alt="student" class="img-thumbnail back-none">
+        </div>
+    </div>
+</div>
+</div>
+
+      </div>
+      <div class="container second-container">
+        <p class=" text-center">  <span class="first-line font-weight-bold">Learn From more than <span class="red">160 member universities</span></span>
+        </p>
+          <p class=" text-center">  <span class="second-line font-weight-bold">
+        Achieve your goals with elearning</span>    </p>
+      </div>
+      <div class="third-container">
+          <div class="heading">
+           <p class=" text-center">
+            <span class="tc1-line font-weight-bold">
+                Why elearning</span>
+           </p> 
+          </div>
+          <div class="container">
+              <div class="row">
+                  <div class="col-4">   
+                       <img src="img/tc-im1.png"  width="100%" class="img-thumbnail">
+                       <p class=" text-center font-weight-bold">   <span class="alin">Top Quality</span></p>
+                    </div>
+           
+                  <div class="col-4"> 
+                        <img src="img/tc-img2.png" width="100%" class="img-thumbnail">
+                        <p class=" text-center font-weight-bold">   <span class="alin">Accessible</span></p>
+
+                    </div>
+                    <div class="col-4"> 
+                        
+                        <img src="img/tc-img3.png" width="100%" class="img-thumbnail">
+                        <p class=" text-center font-weight-bold">   <span class="alin">Applied Learning</span></p>
+
+                    </div>
+               
+                   
+              </div>
+            
+              </div>
+         
+</div>
+
+<div class="container" id="four-container">
+    <div class="container" id="margin-container">
+        <div class="row">
+            <div class="col-4">
+                <img src="img/four1.png" width="130%">
+                
+                 </div> 
+                 <div class="col" id="content">
+                  
+                <span class="font-weight-bold four-line" >Learn from leading universities and companies</span>
+                <br><span class="four-linesmall">start stretching on demand video lectures today from top instructors in subject like bussiness ,computers science,data science language learning and more</span>
+               <br> <button type="button " class=" rc2 btn btn-dark ">Explore       </button>
+                
+                
+                </div>
+        </div>
+        <div class="row">
+           
+                 <div class="col" id="content">
+                  
+                <span class="font-weight-bold four-line" >Find flexible,affordable options</span>
+                <br><span class="four-linesmall">start stretching on demand video lectures today from top instructors in subject like bussiness ,computers science,data science language learning and more</span>
+               <br> <button type="button " class=" rc2 btn btn-dark ">Explore       </button>
+                
+                
+                </div>
+                <div class="col-4">
+                    <img src="img/four2.png" width="130%">
+                    
+                     </div> 
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <img src="img/four3.png" width="130%">
+                
+                 </div> 
+                 <div class="col" id="content">
+                  
+                <span class="font-weight-bold four-line" >Learn from leading universities and companies</span>
+                <br><span class="four-linesmall">start stretching on demand video lectures today from top instructors in subject like bussiness ,computers science,data science language learning and more</span>
+               <br> <button type="button " class=" rc2 btn btn-dark ">Explore       </button>
+                
+                
+                </div>
+        </div>
+        <div class="row">
+            <div class="col" id="content">
+                  
+                <span class="font-weight-bold four-line" >Masters skill with in-depth learning</span>
+                <br><span class="four-linesmall">start stretching on demand video lectures today from top instructors in subject like bussiness ,computers science,data science language learning and more</span>
+               <br> <button type="button " class=" rc2 btn btn-dark ">Explore       </button>
+                
+                
+                </div>
+            <div class="col-4">
+                <img src="img/four4.png" width="130%">
+                
+                 </div> 
+              
+        </div>
+    </div>
+
+ </div>     
+   
+<div class="container fifth">
+ 
+    <img src="img/five.png" class="img-fluid">
+</div>
+<div class="container six">
+ <div class="container">
+    <p class="text-center tc1-line font-weight-bold">Our Vision</p> 
+     <p class="text-center"><span >start stretching on demand video lectures today from top instructors in subject like bussiness ,computers science,data science language learning and more</span>
+     </p>
+   
+ </div>
+ <div class="container fifth">
+    <img src="img/people-section.png" class="img-fluid image1">
+
+    <img src="img/footer.png" class="image img-fluid">
+    <div class="text-block">
+        <span class="font-weight-bold five-line" >Masters skill with in-depth learning</span>
+
+    </div>
+  </div>
+  <div class="container " id="footer">
+    <div class="row justify-content-center">
+<div class="col">
+<span><i class="fa fa-check" aria-hidden="true"></i>Hands on project</span>
+</div>
+<div class="col">
+  <span>  <i class="fa fa-cloud"></i>Support Teams Across The world</span>  
+</div>
+<div class="col">
+   <span><i class="fa fa-lock" aria-hidden="true"></i>Safe and secure online Payment</span> 
+</div>
+
+</div>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
+
